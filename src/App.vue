a0fdcdb7ed1709a090639bd61c1d0c8657efd1a0
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2024-07-05 14:53:42 -0300

    término do formulario

--- src/App.vue
+++ src/App.vue
@@ -16,29 +16,26 @@ const mostrarResultado = ref(false)
 <template>
   <div class="container">
     <form @submit.prevent="mostrarResultado = !mostrarResultado">
-      <div>
+      <div v-if="!mostrarResultado">
         <p>Nome:<input type="text" v-model="nome" placeholder="João" /></p>
         <p>
           Data de nascimento:
-          <input type="text" v-model="nascimento" placeholder="00/00/0000" />
+          <input type="number" v-model="nascimento" placeholder="00/00/0000" />
         </p>
-      </div>
-      <div>
         <p>
           E-mail:
           <input type="email" v-model="email" placeholder="example@example" />
         </p>
         <p>
           Senha:
-          <input type="password" v-model="senha" placeholder="********" minlength="3" />
+          <input type="password" v-model="senha" placeholder="********" minlength="4" />
         </p>
         <p>
           Confirme sua Senha:
-          <input type="password" v-model="confirmar" placeholder="********" />
+          <input type="password" v-model="confirmar" placeholder="********" minlength="4" />
         </p>
-        <p v-if="confirmar == senha && confirmar != ''">A senha está correta</p>
-      </div>
-      <div>
+        <p id="verde" v-if="confirmar == senha && confirmar != ''">A senha está correta</p>
+        <p id="vermelho" v-if="confirmar != senha && confirmar != ''">A senha está incorreta</p>
         <p>
           Endereço:
           <input type="text" v-model="endereço" placeholder="Rua XXX" />
@@ -46,6 +43,10 @@ const mostrarResultado = ref(false)
         <p>
           Cidade:
           <input type="text" v-model="cidade" placeholder="Joinville" />
+        </p>
+        <p>
+          Fale um pouco sobre você:
+          <input type="text" v-model="bio" placeholder="Eu..." />
         </p>
         <label for="estado">Estado: </label>
         <select v-model="estado">
@@ -76,87 +77,54 @@ const mostrarResultado = ref(false)
           <option value="SE">Sergipe</option>
           <option value="TO">Tocantins</option>
         </select>
-      </div>
-      <div class="hobbies">
-        <label for="hobbies">Hobbies favoritos: </label>
-        <div class="sla">
-          <input type="checkbox" v-model="hobbies" value="Esportes" />
-          <p>Esportes</p>
-        </div>
-        <div class="sla">
-          <input type="checkbox" v-model="hobbies" value="Ler" />
-          <p>Ler</p>
-        </div>
-        <div class="sla">
-          <input type="checkbox" v-model="hobbies" value="Música" />
-          <p>Ouvir música</p>
-        </div>
-        <div class="sla">
-          <input type="checkbox" v-model="hobbies" value="Videogames" />
-          <p>Jogar Videogames</p>
-        </div>
-        <div class="sla">
-          <input type="checkbox" v-model="hobbies" value="Assistir" />
-          <p>Assistir TV/Série</p>
-        </div>
-        <div class="sla">
-          <input type="checkbox" v-model="hobbies" value="Redes" />
-          <p>Usar redes sociais</p>
-        </div>
-      </div>
-      <div>
-        <p>
-          Fale um pouco sobre você:
-          <input type="text" v-model="bio" placeholder="Eu..." />
-        </p>
-      </div>
-      <div>
         <button type="submit">Mostrar</button>
+
+        <div class="hobbies">
+          <label for="hobbies">Hobbies favoritos: </label>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Esportes" />
+            <p>Esportes</p>
+          </div>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Ler" />
+            <p>Ler</p>
+          </div>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Música" />
+            <p>Ouvir música</p>
+          </div>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Videogames" />
+            <p>Jogar Videogames</p>
+          </div>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Assistir" />
+            <p>Assistir TV/Série</p>
+          </div>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Redes" />
+            <p>Usar redes sociais</p>
+          </div>
+          <div class="sla">
+            <input type="checkbox" v-model="hobbies" value="Outros" />
+            <p>Outros</p>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="mostrarResultado">
+        <h2>Resultado</h2>
+        <p>Nome: {{ nome }}</p>
+        <p>Email: {{ email }}</p>
+        <p>Senha: {{ senha }}</p>
+        <p>Nascimento: {{ nascimento }}</p>
+        <p>Endereço: {{ endereço }}</p>
+        <p>Cidade: {{ cidade }}</p>
+        <p>Estado: {{ estado }}</p>
+        <p>Hobbies: {{ hobbies.join(', ') }}</p>
+        <p>Biografia: {{ bio }}</p>
+        <button type="submit">Voltar</button>
       </div>
     </form>
-    <div v-if="mostrarResultado" class="resultado">
-      <h2>Resultado</h2>
-      <p>Nome: {{ nome }}</p>
-      <p>Email: {{ email }}</p>
-      <p>Senha: {{ senha }}</p>
-      <p>Nascimento: {{ nascimento }}</p>
-      <p>Endereço: {{ endereço }}</p>
-      <p>Cidade: {{ cidade }}</p>
-      <p>Estado: {{ estado }}</p>
-      <p>Hobbies: {{ hobbies.join(', ') }}</p>
-      <p>Biografia: {{ bio }}</p>
-      <p>{{ mostrarResultado }}</p>
-    </div>
   </div>
 </template>
-<style scoped>
-form {
-  background-color: rgb(187, 163, 202);
-  color: white;
-  display: flex;
-  box-sizing: border-box;
-  padding: 30px;
-  width: 100vw;
-  border-radius: 15px;
-}
-
-form > * {
-  margin: 20px;
-}
-
-div {
-  padding: 10px;
-}
-.hobbies {
-  margin: 25px;
-  display: block;
-}
-
-.sla {
-  display: flex;
-  gap: 10px;
-}
-input {
-  border-radius: 6px;
-}
-</style>
